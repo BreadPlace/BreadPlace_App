82fe819d6688d1df609954c71000961db52d58e4
kang <kangsworkspace@gmail.com>
2025-05-15 17:19:45 +0900

    feat: BreadPlace 타이틀 뷰 추가

---
+++ lib/ui/common_widgets/common_breadplace_title_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'package:bread_place/config/constants/app_colors.dart';
+
+
+class BreadPlaceTitleView extends StatelessWidget {
+  final String title;
+  final String? titleImageAsset;
+  final IconData? trailingIcon;
+  final VoidCallback? onTrailingTap;
+
+  const BreadPlaceTitleView({
+    required this.title,
+    this.titleImageAsset,
+    this.trailingIcon,
+    this.onTrailingTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      child: SizedBox(
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (titleImageAsset != null) ...[
+                  Image.asset(titleImageAsset!, width: 44, height: 44),
+                  const SizedBox(width: 4),
+                ],
+
+                const SizedBox(width: 4),
+
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: AppColors.primary,
+                    fontFamily: 'bmJua',
+                    fontSize: 32,
+                  ),
+                ),
+              ],
+            ),
+
+            if (trailingIcon != null)
+              Positioned(
+                right: 16,
+                child: IconButton(
+                  onPressed: onTrailingTap,
+                  icon: Icon(trailingIcon),
+                  iconSize: 28,
+                  color: AppColors.black,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
