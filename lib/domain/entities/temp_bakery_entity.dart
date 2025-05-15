4d62f8b7ba99ec1e97ecac64355484bbcd3bb9df
kang <kangsworkspace@gmail.com>
2025-05-15 17:19:19 +0900

    feat: 임시 베이커리 Entity 추가

---
+++ lib/domain/entities/temp_bakery_entity.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:bread_place/ui/home/bloc/latlng_extension.dart';
+
+
+class TempBakeryEntity extends Equatable {
+  final String id;
+  final String name;
+  final String address;
+  final LatLng location;
+  final String imagePath;
+
+  const TempBakeryEntity({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.location,
+    required this.imagePath,
+  });
+
+  static const empty = TempBakeryEntity(
+    id: '',
+    name: '',
+    address: '',
+    location: AppLocations.seoulStation,
+    imagePath: 'assets/images/Croissant.png',
+  );
+
+  @override
+  List<Object?> get props => [id, name, address, location];
+}
+
+extension TempBakeryDistanceExtension on TempBakeryEntity {
+  double distanceFromUser(LatLng userLocation) {
+    double distance = Geolocator.distanceBetween(
+      location.latitude,
+      location.longitude,
+      userLocation.latitude,
+      userLocation.longitude,
+    );
+
+    double distanceInKm = distance / 1000;
+    return double.parse(distanceInKm.toStringAsFixed(2));
+  }
+}
