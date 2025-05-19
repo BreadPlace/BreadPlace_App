a20326b41587793b4da247065b097dd522a96fb2
joy293 <opendoor2026@gmail.com>
2025-05-19 16:42:10 +0900

    feat: 검색시 현재 사용자위치 좌표값 넘겨주기

--- lib/ui/search/bloc/search_bloc.dart
+++ lib/ui/search/bloc/search_bloc.dart
@@ -2,23 +2,30 @@ import 'package:bread_place/domain/repositories/kakao_search_repository.dart';
 import 'package:bread_place/ui/search/bloc/search_event.dart';
 import 'package:bread_place/ui/search/bloc/search_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   final KakaoSearchRepository _repository;
 
   SearchBloc(this._repository) : super(SearchInitial()) {
-   on<SearchPlace>(getSearchPlace);
+    on<SearchPlace>(getSearchPlace);
   }
 
-
-  Future<void> getSearchPlace(SearchPlace event, Emitter<SearchState> emit) async {
+  Future<void> getSearchPlace(
+    SearchPlace event,
+    Emitter<SearchState> emit,
+  ) async {
     emit(SearchLoading());
 
-    try {
-      final results = await _repository.searchPlaces(event.keyword);
-      emit(SearchSuccess(results));
-    } catch (e) {
+    final currentPosition = await Geolocator.getCurrentPosition();
 
-    }
+    try {
+      final results = await _repository.searchPlaces(
+        event.keyword,
+        currentPosition.longitude.toString(),
+        currentPosition.latitude.toString(),
+      );
+      emit(SearchSuccess(results));
+    } catch (e) {}
   }
 }
