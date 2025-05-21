fd8bee921b865ddf1a19f4190f037fa4c14f7835
joy293 <opendoor2026@gmail.com>
2025-05-21 19:46:39 +0900

    feat: 요청값 담을 DTO 추가

---
+++ lib/data/dto/request/google/place/text_query_request.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'text_query_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TextQueryRequest _$TextQueryRequestFromJson(Map<String, dynamic> json) =>
+    TextQueryRequest(textQuery: json['textQuery'] as String);
+
+Map<String, dynamic> _$TextQueryRequestToJson(TextQueryRequest instance) =>
+    <String, dynamic>{'textQuery': instance.textQuery};
