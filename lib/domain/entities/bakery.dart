87542d01a941a60ad5647606663e60e28c11cebd
joy293 <opendoor2026@gmail.com>
2025-05-22 23:46:37 +0900

    refactor: Bakery 도메인 엔티티 구조 변경 및 하위 객체 도입

--- lib/domain/entities/bakery.dart
+++ lib/domain/entities/bakery.dart
@@ -3,13 +3,13 @@ class Bakery {
   final String languageCode;
   final String formattedAddress;
   final String formattedPhoneNumber;
-  final String location;
-  final String viewPort;
+  final Location location;
+  final Viewport viewport;
   final String id;
-  final String plusCode;
+  final PlusCode plusCode;
   final String uri; // googleMapsUri
   final List<String> types;
-  final List<String> photos;
+  final String photos;
 
   Bakery({
     required this.displayName,
@@ -17,11 +17,33 @@ class Bakery {
     required this.formattedAddress,
     required this.formattedPhoneNumber,
     required this.location,
-    required this.viewPort,
+    required this.viewport,
     required this.id,
     required this.plusCode,
     required this.uri,
     required this.types,
     required this.photos,
   });
+}
+
+
+class Location {
+  final double latitude;
+  final double longitude;
+
+  Location({required this.latitude, required this.longitude});
+}
+
+class Viewport {
+  final Location low;
+  final Location high;
+
+  Viewport({required this.low, required this.high});
+}
+
+class PlusCode {
+  final String globalCode;
+  final String compoundCode;
+
+  PlusCode({required this.globalCode, required this.compoundCode});
 }
