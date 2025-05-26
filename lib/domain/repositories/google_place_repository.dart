6f8bfd45b002cccf74a093941f5cc6e2bc3fae68
joy293 <opendoor2026@gmail.com>
2025-05-26 20:21:56 +0900

    feat: PlacePhoto 도메인 레포지토리 인터페이스 정의

--- lib/domain/repositories/google_place_repository.dart
+++ lib/domain/repositories/google_place_repository.dart
@@ -4,4 +4,5 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 abstract class GooglePlaceRepository {
   Future<List<Bakery>> searchText(String query);
   Future<List<Bakery>> searchNearby(LatLng searchLocation);
+  Future<String> getPlacePhotoUri(String photoName);
 }
