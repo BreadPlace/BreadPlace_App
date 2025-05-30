f6be550ca75d30d44ebed3fdda7a7012015db684
joy293 <opendoor2026@gmail.com>
2025-05-30 16:13:31 +0900

    feat: kakao flutter sdk 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:bread_place/config/routing/router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:bread_place/config/di/locator.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
+
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']
+  );
+
   initLocator();
   runApp(const MyApp());
 }
