dbfb32036b1d1896c728d87d047bff12218b5098
kang <kangsworkspace@gmail.com>
2025-05-31 16:56:15 +0900

    refactor: searchNearby 엔드포인트 설정

--- lib/data/services/api/google/google_place_endpoint.dart
+++ lib/data/services/api/google/google_place_endpoint.dart
@@ -1,4 +1,5 @@
 class GooglePlaceEndpoint {
   static const searchText = 'v1/places:searchText';
   static const getPlacePhotoUri = 'v1/{name}/media';
+  static const searchNearby = 'v1/places:searchNearby';
 }
