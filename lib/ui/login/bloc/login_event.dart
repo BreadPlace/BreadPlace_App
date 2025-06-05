1b76f0573d86765eaf7a89c62aee2ca708f2a228
joy293 <opendoor2026@gmail.com>
2025-06-05 16:46:58 +0900

    feat: 신규유저가 로그인 했을 경우, 닉네임 설정화면으로 이동

--- lib/ui/login/bloc/login_event.dart
+++ lib/ui/login/bloc/login_event.dart
@@ -8,3 +8,21 @@ sealed class LoginEvent extends Equatable {
 class LoggedOut extends LoginEvent {}
 class CheckAuthStatus extends LoginEvent {}
 class LoginWithKakaoRequested extends LoginEvent {}
+
+class LoginCanceled extends LoginEvent {}
+
+// 닉네임 입력 시작
+class NicknameInputStarted extends LoginEvent {}
+
+// 닉네임 입력 취소
+class NicknameInputCanceled extends LoginEvent {}
+
+// 닉네임 입력 후 저장 요청
+class NicknameSubmitted extends LoginEvent {
+  final String nickname;
+
+  NicknameSubmitted(this.nickname);
+
+  @override
+  List<Object?> get props => [nickname];
+}
