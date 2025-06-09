a0e8d660b37acb63bd1c47d1d9c8a4fbfd7352d3
joy293 <opendoor2026@gmail.com>
2025-06-09 20:51:06 +0900

    feat: 알림관련 권한추가 - 안드로이드

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:bread_place/config/routing/router.dart';
 import 'package:bread_place/ui/login/bloc/login_bloc.dart';
 import 'package:bread_place/config/di/locator.dart';
+import 'data/services/firebase/fcm_notification_service.dart';
 import 'data/services/local/user_local_storage.dart';
-import 'firebase_options.dart';
+import 'package:bread_place/data/services/notification/local_notification_service.dart';
 
+import 'firebase_options.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   await _initializeApp();
 
   runApp(const MyApp());
@@ -41,6 +47,10 @@ Future<void> _initializeApp() async {
   _initKakaoSdk();
   await _initFirebase();
   initLocator();
+  _initLocalNotification();
+
+  // main 함수에서도 한번, 핸들러에서도 한번 총 두번 초기화가 일어납니다.
+  // await _initFcmNotification();
 }
 
 Future<void> _initEnvFile() async {
@@ -58,3 +68,16 @@ Future<void> _initFirebase() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 }
+
+Future<void> _initLocalNotification() async {
+  await LocalNotificationService.init();
+}
+
+// fcm
+// Future<void> _initFcmNotification() async {
+//   final fcmService = FcmNotificationService();
+//   await fcmService.requestPermission(); // 알림 권한 요청
+//
+//   fcmService.getFcmToken();
+//   fcmService.setupForegroundMessaging();
+// }
--- lib/ui/mypage/mypage_screen_main.dart
+++ lib/ui/mypage/mypage_screen_main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:bread_place/data/services/notification/local_notification_service.dart';
+import 'package:bread_place/ui/common_widgets/common_dialog.dart';
+
+import 'package:go_router/go_router.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 class MypageScreenMain extends StatefulWidget {
   const MypageScreenMain({super.key});
 
@@ -9,7 +15,59 @@ class MypageScreenMain extends StatefulWidget {
 
 class _MypageScreenMainState extends State<MypageScreenMain> {
   @override
+  void initState() {
+    super.initState();
+
+    // 첫 프레임 렌더링 이후 권한 체크 및 다이얼로그 호출
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkNotificationPermission();
+    });
+  }
+
+  Future<void> _checkNotificationPermission() async {
+    final granted = await LocalNotificationService.ensurePermissionGranted();
+
+    if (!mounted) return;
+
+    if (!granted) {
+      showDialog(
+        context: context,
+        builder:
+            (_) => CommonDialog(
+              title: '알람 권한 필요',
+              content: '알림 기능을 사용하려면 권한을 허용해주세요. 휴대폰 설정 화면으로 이동하시겠습니까?',
+              onTapNegativeButton: () {
+                context.pop();
+              },
+              onTapPositiveButton: () {
+                openAppSettings();
+              },
+              positiveButtonText: '이동',
+            ),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(children: [Expanded(child: Text('MypageScreenMain'))]);
+    return Column(
+      children: [
+        TextButton(
+          onPressed: () async {
+            print('3초 뒤 알림 예정...');
+
+            // 백그라운드로 전환할 시간 확보
+            Future.delayed(const Duration(seconds: 3), () {
+              LocalNotificationService.showNotification(
+                body: '테스트 알림',
+                title: '',
+                payload: '',
+              );
+            });
+          },
+          child: const Text("3초 뒤 PUSH 알림 테스트"),
+        ),
+      ],
+    );
   }
 }
