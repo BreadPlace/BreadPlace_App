cd078a0b881469e37a34cecce95a36c418acc7d2
joy293 <opendoor2026@gmail.com>
2025-06-18 14:13:42 +0900

    feat: 로컬 저장소에 유저 닉네임 저장기능 추가

--- lib/domain/repositories/user_local_storage_repository.dart
+++ lib/domain/repositories/user_local_storage_repository.dart
@@ -2,4 +2,7 @@ abstract class UserLocalStorageRepository {
   Future<void> saveUserId(String userId);
   Future<String?> getUserId();
   Future<void> removeUserId();
+  Future<void> saveUserNickname(String userNickname);
+  Future<String?> getUserNickname();
+  Future<void> removeUserNickname();
 }
