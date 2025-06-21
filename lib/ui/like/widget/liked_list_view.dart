4e8d136f3e8ce48fa07a048ecfb172bac0a14f2f
joy293 <opendoor2026@gmail.com>
2025-06-22 07:49:17 +0900

    feat: 좋아요 화면에서 특정 베이커리 클릭 시, placeId 로 장소 검색 후 상세화면으로 이동

--- lib/ui/like/widget/liked_list_view.dart
+++ lib/ui/like/widget/liked_list_view.dart
@@ -6,6 +6,9 @@ import 'package:bread_place/ui/like/bloc/like_bloc.dart';
 import 'package:bread_place/ui/like/widget/liked_bakery_container.dart';
 import 'package:bread_place/ui/like/bloc/like_event.dart';
 import 'package:bread_place/ui/common_widgets/common_dialog.dart';
+import 'package:bread_place/ui/search/bloc/search_bloc.dart';
+import 'package:bread_place/ui/search/bloc/search_event.dart';
+import 'package:bread_place/ui/search/bloc/search_state.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -20,8 +23,9 @@ class LikedListView extends StatefulWidget {
 class _LikedListViewState extends State<LikedListView> {
   final isNotified = false;
 
-  void _onSelectBakery(Bakery bakery) {
-    context.push(Routes.bakeryDetail, extra: bakery);
+  // 베이커리 클릭 시, 검색 트리거
+  void _onBakeryContainerTapped(Bakery bakery) {
+    context.read<SearchBloc>().add(SearchPlaceById(placeId: bakery.id));
   }
 
   void _onHeartButtonTapped(Bakery bakery) {
@@ -31,8 +35,7 @@ class _LikedListViewState extends State<LikedListView> {
     );
   }
 
-  void _onNotifyButtonTapped() {
-  }
+  void _onNotifyButtonTapped() {}
 
   void _showRemoveDialog(BuildContext context, Bakery bakery) {
     showDialog(
@@ -45,27 +48,39 @@ class _LikedListViewState extends State<LikedListView> {
   Widget build(BuildContext context) {
     final likes = context.select((LikeBloc bloc) => bloc.state.bakeries);
 
-    return ListView.builder(
-      padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
-      itemCount: likes.length,
-      itemBuilder: (context, index) {
-        final likedBakery = likes[index];
-        final bakery = likedBakery.bakery;
-        final notify = likedBakery.isNotify;
-
-        if (bakery == null) return const SizedBox.shrink(); // null 방지
-
-        return LikedBakeryContainer(
-          bakery: bakery,
-          onTapContainer: () => _onSelectBakery(bakery),
-          onHeartPressed: () => _showRemoveDialog(context, bakery),
-          onNotificationPressed: () => _onNotifyButtonTapped(),
-          isNotified: notify,
-        );
+    return BlocListener<SearchBloc, SearchState>(
+      listener: (context, state) {
+        // 검색 결과가 있으면 상세 페이지로 이동
+        if (state is SearchSuccess && state.bakeries.isNotEmpty) {
+          final bakery = state.bakeries.first;
+          context.push(Routes.bakeryDetail, extra: bakery);
+        } else {
+          SnackBar(content: Text('빵집 정보 없음'));
+        }
       },
+      child: ListView.builder(
+        padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
+        itemCount: likes.length,
+        itemBuilder: (context, index) {
+          final likedBakery = likes[index];
+          final bakery = likedBakery.bakery;
+          final notify = likedBakery.isNotify;
+
+          if (bakery == null) return const SizedBox.shrink(); // null 방지
+
+          return LikedBakeryContainer(
+            bakery: bakery,
+            onTapContainer: () => _onBakeryContainerTapped(bakery),
+            onHeartPressed: () => _showRemoveDialog(context, bakery),
+            onNotificationPressed: () => _onNotifyButtonTapped(),
+            isNotified: notify,
+          );
+        },
+      ),
     );
   }
 
+  // 좋아요 취소 확인 다이얼로그
   Widget _buildRemoveDialog(BuildContext context, Bakery bakery) {
     return CommonDialog(
       content: '해당 빵집이 좋아요 목록에서 사라집니다. 정말 삭제하시겠습니까?',
