23684c790faefbf5e836ab9be686ad28f7cabedb
kangsworkspace <kangsworkspace@gmail.com>
2025-06-28 01:42:34 +0900

    feat: 베이커리 리뷰 DTO, Entity 추가

---
+++ lib/data/dto/response/firebase/bakery_review_dto.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bakery_review_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BakeryReviewDto _$BakeryReviewDtoFromJson(Map<String, dynamic> json) =>
+    BakeryReviewDto(
+      bakeryId: json['bakeryId'] as String,
+      writerId: json['writerId'] as String,
+      writerNickName: json['writerNickName'] as String,
+      recommendBread: json['recommendBread'] as String,
+      rating: (json['rating'] as num).toInt(),
+      reviewText: json['reviewText'] as String,
+      imageUrl: json['imageUrl'] as String?,
+      createdAt: json['createdAt'] as String,
+    );
+
+Map<String, dynamic> _$BakeryReviewDtoToJson(BakeryReviewDto instance) =>
+    <String, dynamic>{
+      'bakeryId': instance.bakeryId,
+      'writerId': instance.writerId,
+      'writerNickName': instance.writerNickName,
+      'recommendBread': instance.recommendBread,
+      'rating': instance.rating,
+      'reviewText': instance.reviewText,
+      'imageUrl': instance.imageUrl,
+      'createdAt': instance.createdAt,
+    };
---
+++ lib/domain/entities/bakery_review_entity.dart
@@ -0,0 +1,21 @@
+class BakeryReviewEntity {
+  final String bakeryId;
+  final String writerId;
+  final String writerNickName;
+  final String recommendBread;
+  final int rating;
+  final String reviewContent;
+  final String? imageUrl;
+  final String createdAt;
+
+  BakeryReviewEntity({
+    required this.bakeryId,
+    required this.writerId,
+    required this.writerNickName,
+    required this.recommendBread,
+    required this.rating,
+    required this.reviewContent,
+    this.imageUrl,
+    required this.createdAt,
+  });
+}
