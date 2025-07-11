4eca85eebe962aefce01a80e44b12555d35199f5
kangsworkspace <kangsworkspace@gmail.com>
2025-07-11 14:47:39 +0900

    refactor: UserLocationUseCase에 PermissionRepository 적용

--- lib/domain/usecases/user_location_use_case.dart
+++ lib/domain/usecases/user_location_use_case.dart
@@ -1,19 +1,23 @@
+import 'package:bread_place/config/constants/app_permission_exception.dart';
+import 'package:bread_place/domain/entities/app_permission.dart';
+import 'package:bread_place/domain/repositories/permission_repository.dart';
 import 'package:bread_place/domain/repositories/user_location_repository.dart';
 
 class UserLocationUseCase {
   final UserLocationRepository _userLocationRepository;
+  final PermissionRepository _permissionRepository;
 
   UserLocationUseCase({
-    required UserLocationRepository userLocationRepository
-  }): _userLocationRepository = userLocationRepository;
+    required UserLocationRepository userLocationRepository,
+    required PermissionRepository permissionRepository,
+  }) : _userLocationRepository = userLocationRepository,
+       _permissionRepository = permissionRepository;
 
   Future<({double latitude, double longitude})>getUserLocation() async {
-    // TODO: 권한 관련 처리는 추후 permissionRepository로 처리해야 합니다.
-    // 권한 확인
-    final hasPermission = true;
+    final permissionStatus = await _permissionRepository.getPermissionStatus(AppPermission.location);
 
-    if(!hasPermission){
-      throw Exception("권한이 없어용");
+    if(permissionStatus != AppPermissionStatus.granted){
+      throw AppPermissionDeniedException();
     }
 
     return _userLocationRepository.getUserLocation();
