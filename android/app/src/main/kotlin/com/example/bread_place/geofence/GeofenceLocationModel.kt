60d8312a2d68b04ef78d75fa8beeba08eb8c016d
joy293 <opendoor2026@gmail.com>
2025-07-15 18:10:12 +0900

    feat: 지오펜스 정보를 담을 model 생성

---
+++ android/app/src/main/kotlin/com/example/bread_place/geofence/GeofenceLocationModel.kt
@@ -0,0 +1,8 @@
+package com.example.bread_place.geofence
+
+data class GeofenceLocationModel(
+    val placeId: String,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double
+)
