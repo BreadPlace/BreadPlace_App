cbc544c32264fc066e765a026f1722729e7d4a5b
kangsworkspace <kangsworkspace@gmail.com>
2025-07-17 00:57:28 +0900

    refactor: 지오펜스 등록/리턴 값 베이커리 ID로 변경

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -26,7 +26,7 @@ import GoogleMaps
         
         
         // MARK: Method Channels
-        
+        // Method Channel을 사용하기 위한 Controller 선언
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         
         let regionMonitoringChannelName: String = "com.bread_place.geofencing/method"
@@ -42,40 +42,29 @@ import GoogleMaps
             binaryMessenger: controller.binaryMessenger
         )
 
-        var regionService = RegionMonitoringService(
+        let regionService = RegionMonitoringService(
             methodChannel: regionMonitoringChannel,
             eventChannel: regionMonitoringEventChannel
         )
 
         regionMonitoringChannel.setMethodCallHandler { [weak self] call, result in
-            guard let self = self else { return }
+            guard let self else { return }
             
             switch call.method {
             case "setGeofencingLocation":
-                // 전달받은 값이 문자열 배열이 아닌 경우 리턴
+                // 전달받은 값이 문자열 배열이 아닌 경우 플러터 에러 리턴
                 guard let stringList = call.arguments as? [String] else {
                     result(FlutterError(code: "INVALID_ARGUMENT", message: "Expected list of strings", details: nil))
                     return
                 }
-                
-                // 전달받은 값 좌표값으로 디코딩
-                let coordinates: [CLLocationCoordinate2D] = stringList.compactMap { point in
-                    let parts = point.split(separator: ",").map{$0.trimmingCharacters(in: .whitespaces)}
-                    
-                    guard parts.count == 2,
-                          let latitude = Double(parts[0]),
-                          let longitude = Double(parts[1])
-                    else {
-                        return nil
-                    }
-                    
-                    return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                }
+
+                // RegionInfo 타입으로 디코딩
+                let regionInfos = self.decodeToRegionInfos(stringList: stringList)
                 
                 // 좌표값으로 지오펜싱 동작
-                regionService.startMonitoringRegions(coordinates: coordinates)
+                regionService.startMonitoringRegions(regionInfos: regionInfos)
                 result("Region set")
-                
+        
             case "stopGeofencingLocation":
                 regionService.stopMonitoringAllRegions()
                 result("Region stop")
@@ -87,5 +76,26 @@ import GoogleMaps
         
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+}
+
+extension AppDelegate {
+    /// ["ID|Name|Latitude|Longitude"] 형태의 배열을 RegionInfo 타입으로 디코딩하는 함수
+    func decodeToRegionInfos(stringList: [String]) -> [RegionInfo] {
+        return stringList.compactMap { deliveredString in
+            let parts = deliveredString.split(separator: "|")
+            
+            guard parts.count == 4,
+                  let latitude = Double(parts[2]),
+                  let longitude = Double(parts[3])
+            else {
+                return nil
+            }
+            
+            let id = String(parts[0])
+            let name = String(parts[1])
+            
+            return RegionInfo(id: id, name: name, latitude: latitude, longitude: longitude)
+        }
     }
 }
