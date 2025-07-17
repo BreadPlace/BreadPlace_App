25ae0c60ad129f07ffeb87647905d0192e991a03
joy293 <opendoor2026@gmail.com>
2025-07-17 20:35:21 +0900

    feat: Onpressed 값이 null 일 때 버튼 비활성화 되도록 수정

--- lib/ui/common_widgets/primary_button.dart
+++ lib/ui/common_widgets/primary_button.dart
@@ -4,36 +4,40 @@ import 'package:flutter/material.dart';
 
 class PrimaryButton extends StatelessWidget {
   final String text;
-  final double horiaontalPadding;
-  final VoidCallback onPressed;
+  final double horizontalPadding;
+  final VoidCallback? onPressed; // null 일 때 버튼 비활성화
+  final Color backgroundColor;
+  final Color textColor;
 
   const PrimaryButton({
     super.key,
     required this.text,
     required this.onPressed,
-    this.horiaontalPadding = 20,
+    this.horizontalPadding = 20,
+    this.backgroundColor = AppColors.primary,
+    this.textColor = AppColors.white,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: horiaontalPadding),
+      padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
       child: SizedBox(
         width: double.infinity,
         height: 48,
         child: ElevatedButton(
           onPressed: onPressed,
           style: ElevatedButton.styleFrom(
-            backgroundColor: AppColors.primary,
+            backgroundColor: backgroundColor,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
             ),
           ),
           child: Text(
               text,
-              style: AppTextStyles.pretendardSemiBold.copyWith(
+              style: AppTextStyles.pretendardRegular.copyWith(
                   fontSize: 18,
-                  color: AppColors.white
+                  color: textColor
               ),
           ),
         ),
