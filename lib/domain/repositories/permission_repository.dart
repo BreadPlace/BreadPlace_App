dc9febb12bc0de45eb62891929616045e0603971
joy293 <opendoor2026@gmail.com>
2025-08-07 15:25:30 +0900

    refactor: 권한 요청하는 메서드 하나로 통일

--- lib/domain/repositories/permission_repository.dart
+++ lib/domain/repositories/permission_repository.dart
@@ -2,7 +2,6 @@ import 'package:bread_place/domain/entities/app_permission.dart';
 
 abstract class PermissionRepository {
   Future<AppPermissionStatus> getPermissionStatus(AppPermission permission);
-  Future<AppPermissionStatus> requestPermission(AppPermission permission);
-  Future<bool> ensurePermissionGranted(AppPermission permission);
+  Future<AppPermissionStatus> ensurePermissionGranted(AppPermission permission);
   Future<void> openDeviceAppSettings();
 }
