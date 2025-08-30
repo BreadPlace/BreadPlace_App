2c8a1e7ee7ee7478335e369b8eb023be4c0e53e8
joy293 <opendoor2026@gmail.com>
2025-08-30 11:27:54 +0900

    feat: 권한 체크가 완료되었는지 확인할 PermissionCheckStatus 상태 추가

--- lib/ui/permission/bloc/permission_bloc.dart
+++ lib/ui/permission/bloc/permission_bloc.dart
@@ -45,6 +45,8 @@ class PermissionBloc extends Bloc<PermissionEvent, PermissionState> {
   // 전체 권한 상태 확인
   Future<void> _onCheckAllPermissionStatus(CheckAllPermissionStatus event,
       Emitter<PermissionState> emit,) async {
+    emit(state.copyWith(checkStatus: PermissionCheckStatus.checking));
+
     try {
       final location = await _permissionUseCase.getPermissionStatus(
           AppPermission.location);
@@ -58,6 +60,7 @@ class PermissionBloc extends Bloc<PermissionEvent, PermissionState> {
           locationStatus: location,
           locationAlwaysStatus: locationAlways,
           notificationStatus: notification,
+          checkStatus: PermissionCheckStatus.checked,
         ),
       );
     } catch (e) {
